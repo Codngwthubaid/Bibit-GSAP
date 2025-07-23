7ecc3ccfbe037bf45d67eb09e58d3df7fab17332
Codngwthubaid <codngwthubaid@gmail.com>
2025-07-23 23:57:02 +0530

    final commit

--- server/models/preference.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const preferenceSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  categories: [String],
-  darkMode: { type: Boolean, default: false }
-});
-
-export const Preference = mongoose.model('Preference', preferenceSchema);
